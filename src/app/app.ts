19eb20eab5ca8c935f28d50325ba06b82c8684f4
Niraj Shah <nirajshah950@gmail.com>
2026-06-17 14:30:29 +0530

    Login component

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,14 +1,15 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Login } from './login/login';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [Login],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
   protected readonly title = signal('first-proj');
-  name = "tutorial";;
-  userName = "this is how we do";
+  name = 'tutorial';
+  userName = 'this is how we do';
 }
