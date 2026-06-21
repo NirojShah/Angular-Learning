7eebe9b57b7496588f142a96e4f2e93bb7acd5c3
Niraj Shah <nirajshah950@gmail.com>
2026-06-21 22:28:13 +0530

    Test case .

---
+++ src/app/pipe/custom-name-pipe-pipe.spec.ts
@@ -0,0 +1,8 @@
+import { CustomNamePipePipe } from './custom-name-pipe-pipe';
+
+describe('CustomNamePipePipe', () => {
+  it('create an instance', () => {
+    const pipe = new CustomNamePipePipe();
+    expect(pipe).toBeTruthy();
+  });
+});
