a3a689b3f84a9fe0d61da673fab8ffe215f8a3a9
Niraj Shah <nirajshah950@gmail.com>
2026-06-21 22:29:11 +0530

    Testcase for the customname array

---
+++ src/app/pipe/custom-name-arr-args-pipe-pipe.spec.ts
@@ -0,0 +1,8 @@
+import { CustomNameArrArgsPipePipe } from './custom-name-arr-args-pipe-pipe';
+
+describe('CustomNameArrArgsPipePipe', () => {
+  it('create an instance', () => {
+    const pipe = new CustomNameArrArgsPipePipe();
+    expect(pipe).toBeTruthy();
+  });
+});
