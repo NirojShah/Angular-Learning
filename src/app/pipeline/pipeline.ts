8e8fb51d5da65a1f6a40ea7b27ae4f777452780e
Niraj Shah <nirajshah950@gmail.com>
2026-06-22 15:38:46 +0530

    add ammount in rupees and $ in the ts file.

--- src/app/pipeline/pipeline.ts
+++ src/app/pipeline/pipeline.ts
@@ -1,15 +1,18 @@
-import { UpperCasePipe,DatePipe } from '@angular/common';
+import { UpperCasePipe,DatePipe, CurrencyPipe } from '@angular/common';
 import { Component } from '@angular/core';
 import { CustomNamePipePipe } from '../pipe/custom-name-pipe-pipe';
 import { CustomNameArrArgsPipePipe } from '../pipe/custom-name-arr-args-pipe-pipe';
 
 @Component({
   selector: 'app-pipeline',
-  imports: [UpperCasePipe,DatePipe,CustomNamePipePipe, CustomNameArrArgsPipePipe],
+  imports: [UpperCasePipe,DatePipe,CustomNamePipePipe, CustomNameArrArgsPipePipe, CurrencyPipe],
   templateUrl: './pipeline.html',
   styleUrl: './pipeline.css',
 })
 export class Pipeline {
   name: string = "Niroj Shah"
   date: Date = new Date(); 
+
+  amountInDollar: number = 12;
+  amountInRupees: number = 25;
 }
