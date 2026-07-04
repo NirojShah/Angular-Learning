eaf68e2fbfbd43ad830f04355c271a4acfbfc6ba
Niraj Shah <nirajshah950@gmail.com>
2026-07-04 15:53:41 +0530

    pass the user store in the constructor to use in the html file.

--- src/app/signals-with-store/signals-with-store.ts
+++ src/app/signals-with-store/signals-with-store.ts
@@ -1,5 +1,6 @@
 import { Component, signal } from '@angular/core';
 import { Store } from '../core/store/store';
+import { UserStore } from '../core/user/user.store';
 
 @Component({
   selector: 'app-signals-with-store',
@@ -8,5 +9,8 @@ import { Store } from '../core/store/store';
   styleUrl: './signals-with-store.css',
 })
 export class SignalsWithStore {
-  constructor(public counterStore: Store) {}
+  constructor(
+    public counterStore: Store,
+    public userStore: UserStore,
+  ) {}
 }
