4aa24f21825cd8880176aefb41f605ed0bbfe075
Niraj Shah <nirajshah950@gmail.com>
2026-07-09 14:59:18 +0530

    Export the RouterLinkActive from teh header to be used in the html page.

--- src/app/task-4/header/header.ts
+++ src/app/task-4/header/header.ts
@@ -1,10 +1,10 @@
 import { Component, effect, input, signal } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 import { HeaderContent } from '../home/home';
 
 @Component({
   selector: 'app-header',
-  imports: [RouterLink],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './header.html',
   styleUrl: './header.css',
 })
