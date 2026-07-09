1e38990b3fc082f3a80eeb03a73d12fa583f6bbe
Niraj Shah <nirajshah950@gmail.com>
2026-07-09 14:12:11 +0530

    test file and main ts file for the home page.
    - Defined the interface for the HeaderContnent type.
    
    - defined the signal to accept the header navbar liks.

---
+++ src/app/task-4/home/home.ts
@@ -0,0 +1,40 @@
+import { Component, effect, signal } from '@angular/core';
+import { Header } from '../header/header';
+import { RouterOutlet } from '@angular/router';
+import { Footer } from '../footer/footer';
+
+export interface HeaderContent {
+  title: string;
+  directUrl: string;
+}
+
+@Component({
+  selector: 'app-home',
+  imports: [Header, RouterOutlet, Footer],
+  templateUrl: './home.html',
+  styleUrl: './home.css',
+})
+export class Home {
+  navBarcontent = signal<HeaderContent[]>([]);
+
+  headerItems: HeaderContent[] = [
+    {
+      title: 'about',
+      directUrl: 'about',
+    },
+    {
+      title: 'service',
+      directUrl: 'service',
+    },
+    {
+      title: 'contact',
+      directUrl: 'contact',
+    },
+  ];
+
+  constructor() {
+    effect(() => {
+      this.navBarcontent.set(this.headerItems);
+    });
+  }
+}
