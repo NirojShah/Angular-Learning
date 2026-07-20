bc34a61968c5474a3392e66bc22ce74529ad020b
Niraj Shah <nirajshah950@gmail.com>
2026-07-20 18:30:00 +0530

    new post.ts file for the logic of the post component

--- src/app/api/post/post.ts
+++ src/app/api/post/post.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { User } from '../../APIs/user';
 
 interface UserSignup {
   firstName: string;
@@ -15,4 +16,18 @@ interface UserSignup {
   templateUrl: './post.html',
   styleUrl: './post.css',
 })
-export class Post {}
+export class Post {
+  userService = inject(User);
+  userSignal = signal<UserSignup | null>(null);
+
+  handleSubmit(): void {
+    try {
+      this.userService.postUser(this.userSignal()).subscribe({
+        next: (resp) => {
+          
+        },
+        error: (err) => {},
+      });
+    } catch (err) {}
+  }
+}
